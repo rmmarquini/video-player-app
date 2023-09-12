0aed41cdadd594b9c29dc1144d9d60f231e6d4a0
Rafa Mardegan <rafael.mardegan@routeasy.com.br>
2023-09-12 12:12:36 -0300

    Init Vite project; Create player components; Add and implement Redux functionalities; Create unit tests

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { useCurrentLesson } from '../store/slices/players'
+
+export function Header() {
+  const { currentModule, currentLesson } = useCurrentLesson()
+
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
+      <span className="text-sm text-zinc-400">{currentModule.title}</span>
+    </div>
+  )
+}
---
+++ src/components/Video.tsx
@@ -0,0 +1,25 @@
+import ReactPlayer from 'react-player'
+import { useDispatch } from 'react-redux'
+import { next, useCurrentLesson } from '../store/slices/players'
+
+export function Video() {
+  const dispatch = useDispatch()
+  const { currentLesson } = useCurrentLesson()
+
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
+  return (
+    <div className="w-full bg-zinc-950 aspect-video">
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        controls
+        playing
+        onEnded={handlePlayNext}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
+      />
+    </div>
+  )
+}
